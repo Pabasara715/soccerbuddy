86b3c7d01dc0334e62da64905dd102d764e9eccb
Pabasara715 <pabasaravihanga715@gmail.com>
2023-12-03 12:44:16 +0530

    line chart done progress tracker

--- lib/view/home/Pages/home_view.dart
+++ lib/view/home/Pages/home_view.dart
@@ -13,17 +13,135 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  List<int> showingTooltipOnSpots = [1, 3, 5];
-
-  List<FlSpot> get allSpots => const [
-        FlSpot(0, 1),
-        FlSpot(1, 2),
-        FlSpot(2, 1.5),
-        FlSpot(3, 3),
-        FlSpot(4, 3.5),
-        FlSpot(5, 5),
-        FlSpot(6, 8),
+  LineTouchData get lineTouchData1 => LineTouchData(
+        handleBuiltInTouches: true,
+        touchTooltipData: LineTouchTooltipData(
+          tooltipBgColor: Colors.blueGrey.withOpacity(0.8),
+        ),
+      );
+  List<LineChartBarData> get lineBarsData1 => [
+        lineChartBarData1_1,
       ];
+
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+        isCurved: true,
+        color: Colors.redAccent,
+        gradient: LinearGradient(colors: [
+          Colors.amber.withOpacity(0.6),
+          Colors.greenAccent.withOpacity(0.6)
+        ]),
+        barWidth: 4,
+        isStrokeCapRound: true,
+        dotData: const FlDotData(show: false),
+        belowBarData: BarAreaData(
+          show: true,
+          gradient: LinearGradient(
+            colors: [
+              ColorTween(begin: Colors.amber, end: Colors.deepPurple)
+                  .lerp(0.2)!
+                  .withOpacity(0.2),
+              ColorTween(begin: Colors.deepPurple, end: Colors.blueAccent)
+                  .lerp(0.2)!
+                  .withOpacity(0.2),
+            ],
+          ),
+        ),
+        spots: const [
+          FlSpot(1, 10),
+          FlSpot(2, 30),
+          FlSpot(3, 15),
+          FlSpot(4, 70),
+          FlSpot(5, 34),
+          FlSpot(6, 76),
+          FlSpot(7, 21),
+        ],
+      );
+
+  SideTitles rightTitles() => SideTitles(
+        getTitlesWidget: rightTitleWidgets,
+        showTitles: true,
+        interval: 20,
+        reservedSize: 40,
+      );
+  Widget rightTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.black54,
+      fontSize: 12,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = '0%';
+        break;
+      case 20:
+        text = '20%';
+        break;
+      case 40:
+        text = '40%';
+        break;
+      case 60:
+        text = '60%';
+        break;
+      case 80:
+        text = '80%';
+        break;
+      case 100:
+        text = '100%';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text, style: style, textAlign: TextAlign.center);
+  }
+
+  SideTitles get bottomTitles => SideTitles(
+        showTitles: true,
+        reservedSize: 32,
+        interval: 1,
+        getTitlesWidget: bottomTitleWidgets,
+      );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.black54,
+      fontSize: 12,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 1:
+        text = const Text('Sun', style: style);
+        break;
+      case 2:
+        text = const Text('Mon', style: style);
+        break;
+      case 3:
+        text = const Text('Tue', style: style);
+        break;
+      case 4:
+        text = const Text('Wed', style: style);
+        break;
+      case 5:
+        text = const Text('Thu', style: style);
+        break;
+      case 6:
+        text = const Text('Fri', style: style);
+        break;
+      case 7:
+        text = const Text('Sat', style: style);
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 10,
+      child: text,
+    );
+  }
+
   final user = FirebaseAuth.instance.currentUser!;
   double progress = 0.5;
   String displayName = "user";
@@ -46,12 +164,12 @@ class _HomeViewState extends State<HomeView> {
     var media = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Column(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            child: SafeArea(
+                child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -222,41 +340,116 @@ class _HomeViewState extends State<HomeView> {
                 SizedBox(
                   height: media.width * 0.05,
                 ),
+                const Text(
+                  ' Your Daily Progress',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(
+                  height: media.width * 0.02,
+                ),
                 Container(
-                    height: media.width * 0.4,
+                    padding: const EdgeInsets.only(left: 15),
+                    height: media.width * 0.5,
                     width: double.maxFinite,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(25)),
-                    child: const Stack(
-                      alignment: Alignment.topLeft,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 15),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                ' Your Daily Progress',
-                                style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
+                    child: Stack(alignment: Alignment.topLeft, children: [
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                      LineChart(LineChartData(
+                        lineTouchData: LineTouchData(
+                          enabled: true,
+                          handleBuiltInTouches: false,
+                          touchCallback: (FlTouchEvent event,
+                              LineTouchResponse? response) {
+                            if (response == null ||
+                                response.lineBarSpots == null) {
+                              return;
+                            }
+                          },
+                          mouseCursorResolver: (FlTouchEvent event,
+                              LineTouchResponse? response) {
+                            if (response == null ||
+                                response.lineBarSpots == null) {
+                              return SystemMouseCursors.basic;
+                            }
+                            return SystemMouseCursors.click;
+                          },
+                          getTouchedSpotIndicator: (LineChartBarData barData,
+                              List<int> spotIndexes) {
+                            return spotIndexes.map((index) {
+                              return TouchedSpotIndicatorData(
+                                const FlLine(
+                                  color: Colors.pink,
                                 ),
-                              ),
-                            ],
+                                FlDotData(
+                                  show: true,
+                                  getDotPainter:
+                                      (spot, percent, barData, index) =>
+                                          FlDotCirclePainter(
+                                    radius: 8,
+                                    color: Colors.blueGrey,
+                                    strokeWidth: 2,
+                                    strokeColor: Colors.black,
+                                  ),
+                                ),
+                              );
+                            }).toList();
+                          },
+                          touchTooltipData: LineTouchTooltipData(
+                            tooltipBgColor: Colors.pink,
+                            tooltipRoundedRadius: 8,
+                            getTooltipItems: (List<LineBarSpot> lineBarsSpot) {
+                              return lineBarsSpot.map((lineBarSpot) {
+                                return LineTooltipItem(
+                                  lineBarSpot.y.toString(),
+                                  const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              }).toList();
+                            },
                           ),
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                        ),
+                        lineBarsData: lineBarsData1,
+                        minY: -0.5,
+                        maxY: 100,
+                        titlesData: FlTitlesData(
+                            show: true,
+                            leftTitles: AxisTitles(),
+                            topTitles: AxisTitles(),
+                            bottomTitles: AxisTitles(sideTitles: bottomTitles),
+                            rightTitles: AxisTitles(sideTitles: rightTitles())),
+                        gridData: FlGridData(
+                          show: true,
+                          drawVerticalLine: false,
+                          horizontalInterval: 18,
+                          getDrawingHorizontalLine: (value) {
+                            return FlLine(
+                              color: Colors.grey.withOpacity(0.3),
+                              strokeWidth: 1,
+                            );
+                          },
+                        ),
+                        borderData: FlBorderData(
+                          show: true,
+                          border: Border.all(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      )),
+                    ])),
+              ]),
+        ))));
   }
 }
